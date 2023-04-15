53be41b476ad4203a131fd7f89017487b8bda5d0
nessakodo <nessakodo@gmail.com>
2023-04-15 10:55:11 -0600

    stylistic changes

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="nessakodo"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#ef19f3"
         fontSize={16}
       />
     </Row>
