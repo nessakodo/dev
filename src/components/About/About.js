edd76bb12625fec4868356b3ea1bc5df162f3663
nessakodo <nessakodo@gmail.com>
2023-05-06 20:25:28 -0600

    Deploying to GitHub Pages

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -34,6 +34,14 @@ function About() {
             <img src={laptopImg} alt="about" className="img-fluid" />
           </Col>
         </Row>
+
+
+        <h1 className="project-heading">
+          Live Code <strong className="purple">Music </strong>
+        </h1>
+
+        
+
         <h1 className="project-heading">
           Professional <strong className="purple">Skillset </strong>
         </h1>
